5ac07ad9913e0d00db517cb18b92de528f1fde81
6ixGhad <jeremydefreitas69@gmail.com>
2021-04-20 16:03:19 +0000

    signup

--- App/views/api.py
+++ App/views/api.py
@@ -1,7 +1,12 @@
 from flask import Blueprint, redirect, render_template, request
 
 api_views = Blueprint('api_views', __name__, template_folder='../templates')
-
+from App.controllers import (create_user, SignUp)
 @api_views.route('/', methods=['GET'])
 def get_api_docs():
     return render_template('index.html')
+
+@app_views.route('/signup', methods=['GET'])
+def signup():
+  form = SignUp() # create form object
+  return render_template('signup.html', form=form) # pass form object to template
