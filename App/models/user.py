f4530377aa7a0fd8884fb0ca5907c479591f0148
6ixGhad <jeremydefreitas69@gmail.com>
2021-04-20 18:28:04 +0000

    idk

--- App/models/user.py
+++ App/models/user.py
@@ -5,8 +5,8 @@ import datetime
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    first_name =  db.Column(db.String, nullable=True)
-    last_name =  db.Column(db.String, nullable=True)
+    first_name =  db.Column(db.String, nullable=False)
+    last_name =  db.Column(db.String, nullable=False)
 
     def toDict(self):
         return{
