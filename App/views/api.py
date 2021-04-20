6a666edaaf7a8a9146ac71d7a5dd08213102810b
6ixGhad <jeremydefreitas69@gmail.com>
2021-04-20 19:21:27 +0000

    save

--- App/views/api.py
+++ App/views/api.py
@@ -1,4 +1,9 @@
 from flask import Blueprint, redirect, render_template, request
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import InputRequired, EqualTo, Email
+from wtforms.fields.html5 import EmailField
+from flask_sqlalchemy import SQLAlchemy
 
 api_views = Blueprint('api_views', __name__, template_folder='../templates')
 from App.controllers import ( create_user, SignUp, signupAction )
