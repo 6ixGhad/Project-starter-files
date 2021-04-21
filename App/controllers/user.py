cf02ecb75e635093419385d809a7414f936c0656
6ixGhad <jeremydefreitas69@gmail.com>
2021-04-21 01:35:18 +0000

    login

--- App/controllers/user.py
+++ App/controllers/user.py
@@ -15,14 +15,24 @@ def create_user(firstname, lastname, uwi_id, email, gender, dob):
     return 'new user'
 
 
+class SignUp(FlaskForm):
+    username = StringField('username', validators=[InputRequired()])
+    email = EmailField('email', validators=[Email(), InputRequired()])
+    password = PasswordField('New Password', validators=[InputRequired(), EqualTo('confirm', message='Passwords must match')])
+    confirm  = PasswordField('Repeat Password')
+    submit = SubmitField('Sign Up', render_kw={'class': 'btn waves-effect waves-light white-text'})
 
+class LogIn(FlaskForm):
+    username = StringField('username', validators=[InputRequired()])
+    password = PasswordField('New Password', validators=[InputRequired()])
+    submit = SubmitField('Login', render_kw={'class': 'btn waves-effect waves-light white-text'})
 
 def signupAction():
   form = SignUp() # create form object
   if form.validate_on_submit():
    data = request.json # get data from form submission
-   newuser = User(first_name=data['username'], last_name=data['username']) # create user object
-    #newuser.set_password(data['password']) # set password
+   newuser = User(username=data['username'], email=data['email']) # create user object
+   newuser.set_password(data['password']) # set password
    db.session.add(newuser) # save new user
    db.session.commit()
     #flash('Account Created!')# send message
--- App/views/api.py
+++ App/views/api.py
@@ -6,7 +6,7 @@ from wtforms.fields.html5 import EmailField
 from flask_sqlalchemy import SQLAlchemy
 
 api_views = Blueprint('api_views', __name__, template_folder='../templates')
-from App.controllers import ( create_user, SignUp, signupAction )
+from App.controllers import ( create_user, SignUp, signupAction, LogIn )
 
 @api_views.route('/status', methods=['GET'])
 def get_api_docs():
@@ -15,11 +15,16 @@ def get_api_docs():
 @api_views.route('/signup', methods=['GET'])
 def signup():
   form = SignUp() # create form object
-  return render_template('signup.html', form=form) # pass form object to template
+  return render_template('signup.html',  form=form) # pass form object to template
 
-@api_views.route('/')
+#@api_views.route('/')
+#def index():
+#  return render_template('login.html')
+
+@api_views.route('/', methods=['GET'])
 def index():
-  return render_template('login.html')
+  form = LogIn()
+  return render_template('login.html', form=form)
 
 @api_views.route('/signup', methods=['POST'])
 def signupAction1():
