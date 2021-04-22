55ef55c5bae401739839728fab67fea04782d12e
6ixGhad <jeremydefreitas69@gmail.com>
2021-04-22 18:44:32 +0000

    db

--- App/models/user.py
+++ App/models/user.py
@@ -33,3 +33,35 @@ class User(UserMixin, db.Model):
     #To String method
     def __repr__(self):
         return '<User {}>'.format(self.username) 
+
+class Player(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    first_name= db.Column(db.String(80), nullable=False)
+    second_name= db.Column(db.String(80), nullable=False)
+    assists= db.Column(db.Integer, nullable=False)
+    clean_sheets= db.Column(db.Integer, nullable=False)
+    form= db.Column(db.Integer, nullable=True)
+    goals_conceded= db.Column(db.Integer, nullable=False)
+    goals_scored= db.Column(db.Integer, nullable=False)
+    minutes= db.Column(db.Integer, nullable=False)
+    penalties_saved= db.Column(db.Integer, nullable=False)
+    red_cards= db.Column(db.Integer, nullable=False)
+    saves= db.Column(db.Integer, nullable=True)
+    yellow_cards= db.Column(db.Integer, nullable=False)
+
+
+    def toDict(self):
+      return {
+        "id": self.id,
+        "first_name": self.first_name,
+        "second_name": self.second_name,
+        "assists": self.assists,
+        "clean_sheets": self.clean_sheets,
+        "form": self.form,
+        "goals_conceded": self.goals_conceded,
+        "goals_scored": self.goals_scored,
+        "minutes": self.minutes,
+        "penalties_saved": self.penalties_saved,
+        "red_cards": self.red_cards,
+        "yellow_cards": self.yellow_cards
+      }
---
+++ initDB_copy.py
@@ -0,0 +1,66 @@
+from App.main import app
+from App.models import ( Player, db )
+import csv
+
+
+#def init():
+with open('App/players_raw.csv', mode='r') as csv_file:
+    csv_reader = csv.reader(csv_file, delimiter=',')
+    count = 0
+
+    for row in csv_reader:
+        if count == 0:
+            id = count
+            first_nameP = 0
+            second_nameP = 0
+            assistsP = 0
+            clean_sheetsP = 0
+            formP = 0
+            goals_concededP= 0
+            goals_scoredP= 0
+            minutesP= 0
+            penalties_savedP= 0
+            red_cardsP= 0
+            savesP= 0
+            yellow_cardsP= 0
+
+            for idx, x in enumerate(row): 
+                id = count
+                if x== 'first_name':
+                    first_nameP=idx
+                if x== 'second_name':
+                    second_nameP=idx
+                if x== 'assists':
+                    assistsP=idx
+                if x== 'clean_sheets':
+                    clean_sheetsP=idx
+                if x== 'form':
+                    formP=idx
+                if x== 'goals_conceded':
+                    goals_concededP=idx
+                if x== 'goals_scored':
+                    goals_scoredP=idx
+                if x== 'minutes':
+                    minutesP=idx
+                if x== 'penalties_saved':
+                    penalties_savedP=idx
+                if x== 'red_cards':
+                    red_cardsP=idx
+                if x== 'saves':
+                    savesP=idx
+                if x== 'yellow_cards':
+                    yellow_cardsP=idx
+
+
+
+            count += 1
+
+        else:
+            allPlayers= Player(id=count, first_name=row[first_nameP], second_name=row[second_nameP], assists=row[assistsP], clean_sheets=row[clean_sheetsP], goals_conceded=row[goals_concededP], goals_scored=row[goals_scoredP] ,minutes=row[minutesP],penalties_saved=row[penalties_savedP],red_cards=row[red_cardsP],yellow_cards=row[yellow_cardsP])
+            db.session.add(allPlayers)
+            db.session.commit()
+
+            count += 1
+
+
+print('database initialized!')
